68a13ca852e91e51a4125043f373ba11341c1737
Tom Iovino <nof1983@gmail.com>
2022-06-25 19:40:19 -0400

    7kyu

--- 7kyu/find-the-stray-number.js
+++ 7kyu/find-the-stray-number.js
@@ -15,10 +15,24 @@
 //R:return the single number that is different
 //E:
 
-function stray(numbers) {
-    //find a number in numbers array that filters down to an array of 1 length
-    return numbers.find((e) => numbers.filter((v) => v===e).length === 1); 
-  } 
+// function stray(numbers) {
+//     //find a number in numbers array that filters down to an array of 1 length
+//     return numbers.find((e) => numbers.filter((v) => v===e).length === 1); 
+//   }  apparently this is too recursive for codewars
+
+function stray(numArr){
+  //check num against num+1 and num+2
+  for(let i = 0; i < numArr.length; i++){
+    if(numArr[i] === numArr[i+1]){     //first num same as num+1
+      if(numArr[i] === numArr[i+2])
+        continue;   
+      else
+        return numArr[i+2];
+    } 
+    else if(numArr[i] !== numArr[i+2])
+      return numArr[i];
+  }
+}
 
 findStray([2,2,7]) // 7
 findStray([19,19,19,4]) //4
