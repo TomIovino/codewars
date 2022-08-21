964f672ab1ba6053c2cbee11784b03f38e1d7d26
Tom Iovino <nof1983@gmail.com>
2022-08-21 19:46:25 -0400

    8-21-2022

---
+++ 08-21-2022/cw.js
@@ -0,0 +1,30 @@
+// 8 Kyu
+
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+// 8 Kyu
+
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+// add the value "codewars" to the already defined websites array
+
+websites.push("codewars")
+
+
+// 8 Kyu
+
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+function mystery() {
+    var results = {sanity: 'Hello'};
+    return results;
+  }
