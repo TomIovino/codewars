464b7023f16a918f8eb588a5f416e67692307e55
Tom Iovino <nof1983@gmail.com>
2022-08-22 20:24:20 -0400

    08-22-2022

---
+++ 08-22-2022/cw.js
@@ -0,0 +1,30 @@
+// 8 Kyu
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers){
+    return numbers.map(x => x*=x)
+                  .reduce((acc,x) => acc += x,0);
+  }
+
+
+// 8 Kyu
+
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+// For example(Input --> Output):
+
+// "anyone else" --> 0
+// "Zach" --> 18
+
+function howManyLightsabersDoYouOwn(name) {
+    return name === "Zach" ? 18 : 0;
+  }
