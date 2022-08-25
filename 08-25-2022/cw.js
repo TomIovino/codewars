e98f940c6e7731c7a3bec3a44a4036a2237fd155
Tom Iovino <nof1983@gmail.com>
2022-08-25 17:02:19 -0400

    8 + 7

---
+++ 08-25-2022/cw.js
@@ -0,0 +1,45 @@
+// 8 Kyu
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    return bool ? "Yes" : "No";
+  }
+
+
+// 7 Kyu
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+function DNAStrand(dna){
+    let str = '';
+    
+    for(let i=0; i<dna.length; i++){
+      switch(dna[i]){
+          case 'A':
+            str += 'T';
+            break;
+          case 'T':
+            str += 'A';
+            break;
+          case 'G':
+            str += 'C';
+            break;
+          case 'C':
+            str += 'G';
+            break;
+      }
+    }
+    return str;
+  }
