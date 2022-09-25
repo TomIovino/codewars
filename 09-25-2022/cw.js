3347464285ccaacdb46bdf6d4793754aaba31a78
Tom Iovino <nof1983@gmail.com>
2022-09-25 09:20:04 -0400

    8kyu

---
+++ 09-25-2022/cw.js
@@ -0,0 +1,32 @@
+// 8 kyu
+// Sum without highest and lowest number
+// 80218589% of 5,87916,822 of 47,406user5036852
+//  JavaScript
+// Node v14.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+function sumArray(array) {
+    if(!array || array.length<1) 
+      return 0;
+    
+    array.sort((a,b)=>a-b);
+    array.shift();
+    array.pop();
+    
+    return array.reduce((acc,c)=> acc+=c,0);
+  }
