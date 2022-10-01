685803ab0016b323ed4cd71ad397de7519138124
Tom Iovino <nof1983@gmail.com>
2022-10-01 18:01:46 -0400

    7kyu

--- 8kyu.js
+++ 8kyu.js
@@ -93,3 +93,33 @@ const quarterOf = (month) => {
   else if(month >= 4) return 2;
   else return 1;
 }
+
+// 10/1/2022
+// 7 kyu
+// Count the divisors of a number
+// 37312591% of 3,72414,458 of 42,293tiriana
+//  JavaScript
+// Node v10.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples (input --> output)
+// 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+// 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+function getDivisorsCnt(n){
+  let result = 0;
+  
+  for(let i = n; i > 0; i--){
+    if(n%i == 0)
+      result++
+  }
+  return result;
+}
