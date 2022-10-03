114401e16f9067f34182365755ccf463130ba260
Tom Iovino <nof1983@gmail.com>
2022-10-03 16:31:16 -0400

    7kyu

--- 7kyu.js
+++ 7kyu.js
@@ -56,3 +56,45 @@ function getSum( a,b,tot=0 ){
     
     return tot;
   }
+
+// 10/3/2022
+// 7 kyu
+// The highest profit wins!
+// 3319987% of 5,24521,687 of 55,983bkaes1 Issue Reported
+//  JavaScript
+// Node v14.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// Story
+// Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+// Task
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+// Examples (Input --> Output)
+// [1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+// Remarks
+// All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+// function minMax(arr){
+//   let min = arr[0];
+//   let max = arr[0];
+  
+//   for(let i = 0; i<arr.length; i++){
+//     if(arr[i] > max) max = arr[i];
+//     if(arr[i] < min) min = arr[i];
+//   }
+  
+//   return [min,max];
+// }
+
+// which takes less memory?
+
+const minMax = arr => {
+  arr.sort((a,b)=>a-b);
+  return [arr[0],arr[arr.length-1]];
+};
