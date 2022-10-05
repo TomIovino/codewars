637755a10305dbb214c27c061ef4d3d2b2ec95cf
Tom Iovino <nof1983@gmail.com>
2022-10-05 16:37:44 -0400

    8kyu

--- 8kyu.js
+++ 8kyu.js
@@ -116,3 +116,34 @@ const quarterOf = (month) => {
 function greet (name, owner) {
   return name === owner ? 'Hello boss' : 'Hello guest';
 }
+
+// 10/05/2022
+// 8 kyu
+// Find Maximum and Minimum Values of a List
+// 44514187% of 6,36524,835 of 68,441Goncalerta2 Issues Reported
+//  JavaScript
+// Node v10.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+// Examples (Input -> Output)
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+// Notes
+// You may consider that there will not be any empty arrays/vectors.
+// FUNDAMENTALS
+
+var min = function(list){
+    
+  return Math.min(...list);
+}
+
+var max = function(list){
+  
+  return Math.max(...list);
+}
