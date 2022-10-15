3876f2c3dc29122bf90650dd5fb684659594c960
Tom Iovino <nof1983@gmail.com>
2022-10-15 16:21:03 -0400

    6kyu

--- 6kyu.js
+++ 6kyu.js
@@ -20,3 +20,86 @@
 function arrayDiff(a, b) {
     return a.filter(x => !b.includes(x));
 }
+
+//10/15/2022
+// 6 kyu
+// Give me a Diamond
+// 141926287% of 3,3938,146 of 27,250jayeshcp3 Issues Reported
+//  JavaScript
+// Node v8.1.3
+// VIM
+// EMACS
+// Instructions
+// Output
+// Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+// Task
+// You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+// Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
+
+// Examples
+// A size 3 diamond:
+
+//  *
+// ***
+//  *
+// ...which would appear as a string of " *\n***\n *\n"
+
+// A size 5 diamond:
+
+//   *
+//  ***
+// *****
+//  ***
+//   *
+// ...that is:
+
+// "  *\n ***\n*****\n ***\n  *\n"
+// STRINGSASCII ARTFUNDAMENTALS
+
+function diamond(n){
+    if(n % 2 === 0 || n < 0)
+      return null;
+  
+    let spaceCounter;
+    let diamond = '';
+    
+    spaceCounter = (n-1) / 2;
+    
+    function addspaces(x){
+      let str = '';
+      
+      for(let i=0; i<spaceCounter; i++)
+        str += ' ';
+      
+      return str;
+    }
+    
+    function addstars(x){
+      let str = '';
+      
+      for(let i=0; i<x; i++)
+        str += '*';
+      
+      return str;
+    }
+    
+    for(let i=1; i<=n; i+=2){
+      diamond += addspaces(i);
+      diamond += addstars(i);
+      diamond += '\n';
+      spaceCounter -= 1;
+    }
+    
+    spaceCounter = 1;
+    
+    for(let i=n-2; i>=1; i-=2){
+      diamond += addspaces(i);
+      diamond += addstars(i);
+      diamond += '\n';
+      spaceCounter += 1;  
+    }
+    
+    return diamond;
+  }
