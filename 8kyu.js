daa24086d03351a12bc14c24f0a2027e3eee08e4
Tom Iovino <nof1983@gmail.com>
2022-10-16 16:25:55 -0400

    8kyu

--- 8kyu.js
+++ 8kyu.js
@@ -235,3 +235,26 @@ class Kata {
 function otherAngle(a, b) {
   return 180-(a+b);
 }
+
+// 10/16/2022
+// 8 kyu
+// Grasshopper - Terminal game move function
+// 1125285% of 3,75010,079 of 33,864danleavitt0
+//  JavaScript
+// Node v14.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+// FUNDAMENTALS
+
+function move (position, roll) {
+  return position += roll*2;
+}
