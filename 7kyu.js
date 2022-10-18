f34be1426303c5466d608f527c6069af80146275
Tom Iovino <nof1983@gmail.com>
2022-10-18 14:16:46 -0400

    7kyu

--- 7kyu.js
+++ 7kyu.js
@@ -154,3 +154,46 @@ String.prototype.toJadenCase = function (str) {
 function sortByLength (array) {
   return array.sort((a,b)=>a.length - b.length)
 };
+
+// 10/18/2022
+// 7 kyu
+// Make a function that does arithmetic!
+// 26710188% of 3,48815,172 of 34,972ZooeyMiller2 Issues Reported
+//  JavaScript
+// Node v14.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply".
+
+// A few examples:(Input1, Input2, Input3 --> Output)
+
+// 5, 2, "add"      --> 7
+// 5, 2, "subtract" --> 3
+// 5, 2, "multiply" --> 10
+// 5, 2, "divide"   --> 2.5
+// Try to do it without using if statements!
+
+// FUNDAMENTALS
+
+function arithmetic(a, b, operator){
+  switch (operator){
+    case "add":
+      return a + b;
+      break;
+    case "subtract":
+      return a - b;
+      break;
+    case "multiply":
+      return a * b;
+      break;
+    case "divide":
+      return a / b;
+      break;
+  }
+}
