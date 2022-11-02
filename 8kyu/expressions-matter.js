650aeaa6eeac197991a03b22d2ce46d92a55edb7
Tom Iovino <nof1983@gmail.com>
2022-11-01 20:51:06 -0400

    8kyu

---
+++ 8kyu/expressions-matter.js
@@ -0,0 +1,31 @@
+// 11/1/2022
+// Task
+// Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+// In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
+// Example
+// With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+
+// 1 * (2 + 3) = 5
+// 1 * 2 * 3 = 6
+// 1 + 2 * 3 = 7
+// (1 + 2) * 3 = 9
+// So the maximum value that you can obtain is 9.
+
+// Notes
+// The numbers are always positive.
+// The numbers are in the range (1  ≤  a, b, c  ≤  10).
+// You can use the same operation more than once.
+// It's not necessary to place all the signs and brackets.
+// Repetition in numbers may occur .
+// You cannot swap the operands. For instance, in the given example you cannot get expression (1 + 3) * 2 = 8.
+
+function expressionMatter(a, b, c) {
+    let n1 = (a+b)*c;
+    let n2 = a+b*c;
+    let n3 = a*(b+c);
+    let n4 = a*b+c;
+    let n5 = a*b*c;
+    let n6 = a+b+c;
+
+    return Math.max(n1,n2,n3,n4,n5,n6);
+}
