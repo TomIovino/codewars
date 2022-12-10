91eb666f076200dd4c9aff67da76bc5ca0c927c6
Tom Iovino <nof1983@gmail.com>
2022-12-10 18:05:34 -0500

    8kyu

---
+++ 8kyu/find-the-position.js
@@ -0,0 +1,11 @@
+// 12/10/2022
+
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+function position(letter){
+    return "Position of alphabet: "+('abcdefghijklmnopqrstuvwxyz'.indexOf(letter)+1);
+  }
