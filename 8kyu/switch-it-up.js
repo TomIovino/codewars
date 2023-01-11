b5394637448d5dffab038d232e69a415efbd96c9
Tom Iovino <nof1983@gmail.com>
2023-01-10 19:42:14 -0500

    8kyu

---
+++ 8kyu/switch-it-up.js
@@ -0,0 +1,18 @@
+// 1/10/2023
+
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+function switchItUp(number){
+    const nums = {
+      1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five', 
+      6:'Six', 7:'Seven', 8:'Eight', 9:'Nine', 0:'Zero'
+    }
+    
+    return nums[number];
+  }
