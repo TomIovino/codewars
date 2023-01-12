5fdde4a09a1610fe37efa2b63abc1b2ac0eddb01
Tom Iovino <nof1983@gmail.com>
2023-01-11 19:44:29 -0500

    8kyu

---
+++ 8kyu/regular-ball-super-ball.js
@@ -0,0 +1,20 @@
+// 1/11/2023
+
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+
+class Ball{
+    constructor(type){
+      if(!type)
+        this.ballType = 'regular'
+      else
+        this.ballType = type
+    }
+  }
