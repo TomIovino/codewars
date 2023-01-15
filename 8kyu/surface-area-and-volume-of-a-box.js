fae0eca182d842dd028357de30a2bfc8be87bcc0
Tom Iovino <nof1983@gmail.com>
2023-01-15 13:46:22 -0500

    8kyu

---
+++ 8kyu/surface-area-and-volume-of-a-box.js
@@ -0,0 +1,5 @@
+// 1/15/2023
+
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+const  getSize = (w, h, l) => [(2*l*w) + (2*l*h) + (2*w*h), w*h*l];
