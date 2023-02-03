3e2322aef2cc1a756926a0b3f5b39a4ed52d3134
Tom Iovino <nof1983@gmail.com>
2023-02-02 19:06:03 -0500

    8kyu

---
+++ 8kyu/grasshopper-basic-function-fixer.js
@@ -0,0 +1,8 @@
+// 2/2/2023
+
+// Fix the function
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+const addFive=(n)=>n+5;
