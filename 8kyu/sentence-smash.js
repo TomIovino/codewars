a9cb964cf218e765e50b357516c5985caa14f0bb
Tom Iovino <nof1983@gmail.com>
2023-03-05 21:01:01 -0500

    8kyu

--- 8kyu/sentence-smash.js
+++ 8kyu/sentence-smash.js
@@ -6,6 +6,4 @@
 // Example
 // ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
-function smash (words) {
-    return words.join(' ');
- }
+const smash = (words) => words.join(' ');
