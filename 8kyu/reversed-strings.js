4f423eacb3c9b64834b754fe1b855bb4613df092
Tom Iovino <nof1983@gmail.com>
2023-03-09 19:59:22 -0500

    8kyu

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,10 @@
+// 3/9/2023
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+    return str.split('').reverse().join('');
+  }
