3dbad938c15ecb37f8f1e8aebc3df9952f0d373c
Tom Iovino <nof1983@gmail.com>
2023-04-04 17:27:55 -0400

    8kyu

---
+++ 8kyu/polish-alphabet.js
@@ -0,0 +1,46 @@
+// 4/4/2023
+
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+function correctPolishLetters (string) {
+  
+  const polishLetters = {
+    'ą' : 'a',
+    'ć' : 'c',
+    'ę' : 'e',
+    'ł' : 'l',
+    'ń' : 'n',
+    'ó' : 'o',
+    'ś' : 's',
+    'ź' : 'z',
+    'ż' : 'z'
+  }
+  
+  let correctedStr = '';
+  
+  for(let i=0; i<string.length; i++){
+    if(Object.keys(polishLetters).includes(string[i]))
+      correctedStr += polishLetters[string[i]]
+    else
+      correctedStr += string[i];
+  }
+
+  return correctedStr;
+}
