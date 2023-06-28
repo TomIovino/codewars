a80bfcb07e4d81a1038d36e31b451b3e16ace76e
Tom Iovino <nof1983@gmail.com>
2023-06-27 20:57:22 -0400

    8kyu

---
+++ 8kyu/grasshopper-combine-strings.js
@@ -0,0 +1,15 @@
+// 6/27/2023
+
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+
+function combineNames(first, last){
+    return first+' '+last;
+  }
