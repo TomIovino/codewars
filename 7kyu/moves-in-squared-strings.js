609241bdd09a567867dbdb2629df6575c0a9a40b
Tom Iovino <nof1983@gmail.com>
2023-07-25 19:55:42 -0400

    7kyu

---
+++ 7kyu/moves-in-squared-strings.js
@@ -0,0 +1,52 @@
+// 7/25/2023
+
+// This kata is the first of a sequence of four about "Squared Strings".
+
+// You are given a string of n lines, each substring being n characters long: For example:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+
+// We will study some transformations of this square of strings.
+
+// Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+// vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+// Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+//  hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+// or printed:
+
+// vertical mirror   |horizontal mirror   
+// abcd --> dcba     |abcd --> mnop 
+// efgh     hgfe     |efgh     ijkl 
+// ijkl     lkji     |ijkl     efgh 
+// mnop     ponm     |mnop     abcd 
+// Task:
+// Write these two functions
+// and
+
+// high-order function oper(fct, s) where
+
+// fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+// Examples:
+// s = "abcd\nefgh\nijkl\nmnop"
+// oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+// oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+
+function vertMirror(strng) {
+    // Split the string into individual lines
+    const lines = strng.split('\n');
+    // Reverse each line and join them back
+    return lines.map(line => line.split('').reverse().join('')).join('\n');
+}
+
+function horMirror(strng) {
+    // Split the string into individual lines
+    const lines = strng.split('\n');
+    // Reverse the order of lines and join them back
+    return lines.reverse().join('\n');
+}
+
+function oper(fct, s) {
+    // Call the provided function on the input string
+    return fct(s);
+}
