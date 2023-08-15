ad64371c4b3cc48978969cef2941a04853372d5c
Tom Iovino <nof1983@gmail.com>
2023-08-15 19:57:48 -0400

    8kyu

---
+++ 8kyu/price-of-mangoes.js
@@ -0,0 +1,22 @@
+// 8/15/2023
+
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+// Examples
+// mango(2, 3) ==> 6    # 2 mangoes for $3 per unit = $6; no mango for free
+// mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
+// mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
+// mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free
+
+function mango(quantity, price) {
+    // Calculate the number of mangoes that are part of the offer
+    let offerMangoes = Math.floor(quantity / 3) * 2;
+  
+    // Calculate the number of mangoes that are not part of the offer
+    let remainingMangoes = quantity % 3;
+  
+    // Calculate the total cost based on the offer and remaining mangoes
+    let totalCost = (offerMangoes + remainingMangoes) * price;
+  
+    return totalCost;
+  }
