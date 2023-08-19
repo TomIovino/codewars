b744615be816e9f1e331f93a78a127737ddef498
Tom Iovino <nof1983@gmail.com>
2023-08-19 18:29:45 -0400

    7kyu

---
+++ 7kyu/filter-the-number.js
@@ -0,0 +1,23 @@
+// 8/19/2023
+
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+// Task
+// Your task is to return a number from a string.
+
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+var filterString = function(value) {
+    // Use a regular expression to match all consecutive digits
+    var numbers = value.match(/\d+/g);
+    
+    // Join the matched strings to form a single string
+    var resultString = numbers.join("");
+    
+    // Convert the combined string to a number
+    var result = parseInt(resultString);
+    
+    return result;
+  }
